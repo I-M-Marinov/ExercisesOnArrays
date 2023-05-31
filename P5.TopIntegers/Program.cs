45f94a272e9ff30e5551bc60958bcd6e91acb801
Ivan Marinov <lcfrrr@gmail.com>
2023-05-31 14:19:20 +0300

    aaa

---
+++ P5.TopIntegers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace P5.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int i = 0; i < inputArray.Lenght; i++)
+            {
+                
+            }
+        }
+    }
+}
