ea9058118b8ec7cae58d1b8f96d77c0e47b222b8
I-M-Marinov <lcfrrr@gmail.com>
2023-05-31 06:15:37 -0500

    P5.TopIntergers
    P5.TopIntergers

---
+++ P5.TopIntegers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace P5.TopIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] strongNumbersArray = new int[inputArray.Length];
+
+            int strongNumber = 0;
+
+            for (int i = 0; i < inputArray.Length-1; i++)
+            {
+                if (inputArray[i] > inputArray[i + 1] && i + 1 < inputArray.Length)
+                {
+                    strongNumber = inputArray[i];
+                    strongNumbersArray[i] = strongNumber;
+                }
+
+
+            }
+            Console.WriteLine(string.Join(" ", strongNumbersArray));
+        }
+    }
+}
